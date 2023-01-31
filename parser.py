2d090ea73f16d49578158de9fe4ddf3bc4c42343
Austin Burkeen <45767276+burkeena@users.noreply.github.com>
2023-01-30 23:16:26 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -9,12 +9,23 @@ full_path = os.path.join(absolute_path, relative_path)
 print(absolute_path + "; This is the absolute path")
 print(relative_path + "; This is relative path")
 print(full_path + "; This is the full path")
+print("\n") ## Blank line
 
 json_file = open(relative_path + "file.json") ## Use the relative path, and open the json file
 
-data = json.load(json_file) ## 
+data = json.load(json_file) ## Convert to python dict 
 
-for i in data['certifications']:
-    print(i)
+def print_json():
+    for i in data['certifications']:
+        print(i)
+
+def decode_json():
+    print("\n") ## Blank line
+    print("This is the decoded JSON example:")
+    for cert_names in data['certifications']:
+        print(cert_names['name'],cert_names['courses'])
+
+print_json() # Run print_json function
+decode_json() # Run decode_json function
 
 json_file.close()
