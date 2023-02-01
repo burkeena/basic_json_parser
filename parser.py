d9e057812e45278b53772ece67e6d38f7064ae8f
Austin Burkeen <45767276+burkeena@users.noreply.github.com>
2023-02-01 14:42:20 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,24 +1,27 @@
+
 import json
 import os
 
 ## Get paths and combine to create full path
 absolute_path = os.path.dirname(__file__)
+
 relative_path = "json/"
-file_name = input("What is the file name?") # Accept user input to define file to read
+file_name = input("What is the file name? ") # Accept user input to define file to read
 full_path = os.path.join(absolute_path, relative_path, file_name)
 print("\n") ## Blank line
 
-json_class = input("Enter JSON Class to Extract:")
+json_class = input("Enter JSON Class to Extract: ")
 print("The JSON class you selected is: " + json_class)
 print("\n") ## Blank line
 
 ## Column names 
-column_1 = input("What is column 1's name?:")
-column_2 = input("What is column 2's name?:")
+column_1 = input("What is column 1's name?: ")
+column_2 = input("What is column 2's name?: ")
 
 print(absolute_path + "; This is the absolute path")
 print(relative_path + "; This is relative path")
 print(full_path + "; This is the full path")
+print("\n") ## Blank line
 print("\n") ## Blank line
 
 json_file = open(full_path) ## Use the full path, and open the json file
