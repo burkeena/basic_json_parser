b86e13d1ac70e5fa3b5ed0b2f6133efd2d105890
Austin Burkeen <45767276+burkeena@users.noreply.github.com>
2023-02-01 01:29:33 -0500

    Update parser.py
    Changed full_path to combine file_name, then altered the json open to use the combined full path.

--- parser.py
+++ parser.py
@@ -1,25 +1,28 @@
-
 import json
 import os
+import re
 
 absolute_path = os.path.dirname(__file__)
 relative_path = "json/"
-full_path = os.path.join(absolute_path, relative_path)
+file_name = "file.json"
+full_path = os.path.join(absolute_path, relative_path, file_name)
 
 print(absolute_path + "; This is the absolute path")
 print(relative_path + "; This is relative path")
 print(full_path + "; This is the full path")
 print("\n") ## Blank line
 
-json_file = open(full_path + "file.json") ## Use the full path, and open the json file
+json_file = open(full_path) ## Use the full path, and open the json file
 
 data = json.load(json_file) ## Convert to python dict 
 
 def print_json(): ## Print dictionary function
+    print("\n") ## Blank line
+    print("This is the loaded JSON Sample:")
     for i in data['certifications']:
         print(i)
 
-def decode_json(): ## Decode dictionary 
+def decode_json(): ## Decode dictionary function
     print("\n") ## Blank line
     print("This is the decoded JSON example:")
     for cert_names in data['certifications']:
