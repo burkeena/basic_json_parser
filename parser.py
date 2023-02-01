d8d431b3ce0682bb2c6f1881770feb8a68b34a5a
Austin Burkeen <45767276+burkeena@users.noreply.github.com>
2023-02-01 14:17:16 -0500

    Update parser.py
    Added user input logic

--- parser.py
+++ parser.py
@@ -1,11 +1,20 @@
 import json
 import os
-import re
 
+## Get paths and combine to create full path
 absolute_path = os.path.dirname(__file__)
 relative_path = "json/"
 file_name = "file.json"
 full_path = os.path.join(absolute_path, relative_path, file_name)
+print("\n") ## Blank line
+
+json_class = input("Enter JSON Class to Extract:")
+print("The JSON class you selected is: " + json_class)
+print("\n") ## Blank line
+
+## Column names 
+column_1 = input("What is column 1's name?:")
+column_2 = input("What is column 2's name?:")
 
 print(absolute_path + "; This is the absolute path")
 print(relative_path + "; This is relative path")
@@ -19,16 +28,18 @@ data = json.load(json_file) ## Convert to python dict
 def print_json(): ## Print dictionary function
     print("\n") ## Blank line
     print("This is the loaded JSON Sample:")
-    for i in data['certifications']:
+    for i in data[json_class]:
         print(i)
 
 def decode_json(): ## Decode dictionary function
     print("\n") ## Blank line
     print("This is the decoded JSON example:")
-    for cert_names in data['certifications']:
-        print(cert_names['name'],cert_names['courses'])
+    for cert_names in data[json_class]:
+        print(cert_names[column_1],cert_names[column_2])
+
 
 print_json() # Run print_json function
 decode_json() # Run decode_json function
+
 
 json_file.close()
